e53bac825767a1bb37076eace686e3554d6c6c9d
DA\nwuser <goroumakisimamu810@gmail.com>
2017-03-20 02:04:02 +0900

    ゲームオーバー＆ボタンが表示されるようになった(ボタンを押したときの処理は無し) canvasの isGame_Over を1にすると表示

---
+++ Assets/Scripts/GameOver.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameOver : MonoBehaviour {
+
+    public int isGame_Over;
+
+	// Use this for initialization
+	void Start () {
+
+        var obj = transform.Find("Gameover").gameObject;
+        obj.SetActive(false);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        var obj = transform.Find("Gameover").gameObject;
+        if (isGame_Over == 1)
+        {
+            obj.SetActive(true);
+            isGame_Over = 0;
+        }
+	}
+}
