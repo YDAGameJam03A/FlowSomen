5cee901f473bc6e1fc90a74eb3ec93ce1735e455
DA\nwuser <apoapoapo1023@gmail.com>
2017-03-19 19:16:12 +0900

    画面遷移タイトル→ゲームセレクト ゲームセレクトにイージー・ノーマル・ハードのボタンを設置 HitArea,LostAreaのゲームオブジェクト配置

---
+++ Assets/Titlebutton.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+
+[RequireComponent(typeof(Button))]
+public class Titlebutton : UIBehaviour
+{
+	　protected override void Start()
+	　{
+		　　base.Start();
+
+		　　// Buttonクリック時、OnClickメソッドを呼び出す
+		　　GetComponent<Button>().onClick.AddListener(OnClick);
+	　}
+
+	　void OnClick()
+	　{
+		　　// 「Stageserect」シーンに遷移する
+		　　SceneManager.LoadScene("Stageserect");
+	  }
+}
