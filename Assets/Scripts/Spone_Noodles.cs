00ac58eec1a291bf4e40141859b5d4fa33f9016d
DA\nwuser <goroumakisimamu810@gmail.com>
2017-03-19 17:40:46 +0900

    奥から麺ではない何かが流れてくる

---
+++ Assets/Scripts/Spone_Noodles.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spone_Noodles : MonoBehaviour
+{
+
+    public GameObject Noodles;
+
+    private float time;         //n秒ごとに実行する
+
+
+    void Start()
+    {
+
+        
+    }
+
+    void Update()
+    {
+        time -= Time.deltaTime;
+        if (time <= 0.0)
+        {
+            time = 1.0f;
+            Instantiate(Noodles, new Vector3(0, 3, 0), Quaternion.identity);
+
+        }
+    }
+}
