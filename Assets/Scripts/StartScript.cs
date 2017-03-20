e7ea3e5a069b861c5d238eccebbbad4d881337c0
yuutaro-O <yuutarousoft@gmail.com>
2017-03-20 13:19:03 +0900

    ビルド時にウィンドウの解像度を768x1024にならないのを修正

---
+++ Assets/Scripts/StartScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartScript : MonoBehaviour {
+
+    private void Awake()
+    {
+        Screen.SetResolution(768, 1024,false);
+        SceneManager.LoadScene("Title");
+    }
+    //// Use this for initialization
+    //void Start () {
+
+    //}
+
+    //// Update is called once per frame
+    //void Update () {
+
+    //}
+}
